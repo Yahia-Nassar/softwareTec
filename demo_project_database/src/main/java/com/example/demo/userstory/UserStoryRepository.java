62b94ca0d0cf716cefc8992cfc1cae6b87c965ae
Yahia-Nassar <119878119+Yahia-Nassar@users.noreply.github.com>
2026-01-16 20:13:52 +0200

    Delete demo_project_database directory

--- demo_project_database/src/main/java/com/example/demo/userstory/UserStoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.userstory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserStoryRepository extends JpaRepository<UserStory, Long> {
-}
